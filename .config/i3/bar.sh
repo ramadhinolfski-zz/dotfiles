febabb0c911e3fea1d28c7e9cedaf16b0eeb391d
Ramadhinolfski <hanafiah.ramadhan@gmail.com>
2016-04-28 22:02:29 +0700

    add i3wm theme

---
+++ .config/i3/bar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Ramadhinolfski"
+
+while :
+do
+	sleep 1
+done
---
+++ .config/i3/bin/panel-info.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+ScreenWidth=1366
+fg="#efefef"
+bg="#343C48"
+hg="25"
+icons2="System San Francisco:size=12"
+icons4="System San Francisco:size=10"
+bg_volume="#BA5E57"
+font='System San Francisco-9'
+WidthInfo=`expr $ScreenWidth / 2 - 100`
+WidthVolume=100
+X_info=`expr $ScreenWidth - $WidthInfo - $WidthVolume`
+X_volume=`expr $ScreenWidth - $WidthVolume`
+
+conky -c ~/.config/i3/bin/conky/info | dzen2 -p -ta r -e 'button3=' -fn 'System San Francisco-9' -fg $fg -bg $bg -h $hg -w $WidthInfo -x $X_info &
+conky -c ~/.config/i3/bin/conky/info2 | dzen2 -p -ta r -e 'button3=' -fn 'System San Francisco-9' -fg $fg -bg $bg_volume -h $hg -w $WidthVolume -x $X_volume &
+
